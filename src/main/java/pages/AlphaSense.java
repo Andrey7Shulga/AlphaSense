389f729f1f40644c6e647b239d0caecc0c5a9d39
Andrey Shulga <ray7sun@gmail.com>
2022-01-18 17:47:46 +0300

    add text to searchField

--- src/main/java/pages/AlphaSense.java
+++ src/main/java/pages/AlphaSense.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -8,63 +9,99 @@ import java.time.Duration;
 
 public class AlphaSense {
 
-    private WebDriverWait wait;
-    private JavascriptExecutor js;
+    private final WebDriverWait wait;
+    private final JavascriptExecutor js;
     private final long timeout = 10;
     private WebElement element;
+    private final Actions actions;
 
 
     public AlphaSense(WebDriver driver) {
         wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
         js = (JavascriptExecutor) driver;
+        actions = new Actions(driver);
         if (!driver.getTitle().equals("AlphaSense")) {
             throw new IllegalStateException("The loaded page is not Alpha Sense required Page," +
                     " current page title is: " + driver.getTitle());
         }
     }
 
-//    By searchTextField = By.cssSelector("#searchInDocument");
-//    By searchTextField = By.xpath("//*[contains(@class, 'CodeMirror-placeholder')]");
-    By searchTextField = By.xpath("//span[@role='presentation']");
+    By searchBox = By.cssSelector("[data-location=CustomSearchBox]");
+    By searchResults = By.cssSelector("[role=rowgroup]");
+
+
+    By searchTextField = By.cssSelector("#searchInDocument");
+    By searchTextField_1 = By.xpath("//*[contains(@class, 'CodeMirror-placeholder')]");
+    By searchTextField_2 = By.xpath("//*[@role='presentation']//span[@role]");
 
 
 
 
-    public void insertTextToTextField(String text) {
+    public void sendTextToSearchBoxAndClick(String text) {
         try {
-            element = wait.until(ExpectedConditions.visibilityOfElementLocated(searchTextField));
+            actions.moveToElement(wait.until(ExpectedConditions.elementToBeClickable(searchBox)))
+                    .click()
+                    .sendKeys(text + "\n")
+                    .build()
+                    .perform();
+        } catch (NoSuchElementException | TimeoutException e) {
+            throw new RuntimeException(String.format("Message: %S. Element's locator: '%s'", e, searchBox.toString()));
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void insertTextToTextField_ (String text) {
+        try {
+            element = wait.until(ExpectedConditions.elementToBeClickable(searchTextField));
             System.out.println("Text before: " + element.getText());
-//            element.clear();
             element.sendKeys(text);
-            System.out.println("Text after: " + element.getText());
+            System.out.println("Text after clearing: " + element.getText());
+
         } catch (NoSuchElementException | TimeoutException e) {
             throw new NoSuchElementException(String.format("Element is not found by '%s'", searchTextField.toString()));
         }
     }
 
+//    public void insertTextToTextField(String text) {
+//        try {
+//            element = wait.until(ExpectedConditions.elementToBeClickable(searchTextField_1));
+//            System.out.println("Text before: " + element.getText());
+//            element.clear();
+//            System.out.println("Text after clearing: " + element.getText());
+//
+//            WebElement element2 = wait.until(ExpectedConditions.elementToBeClickable(searchTextField_2));
+//            System.out.println("Text before: " + element2.getText());
+//            element2.sendKeys(text);
+//            System.out.println("Text after typing: " + element2.getText());
+//        } catch (NoSuchElementException | TimeoutException e) {
+//            throw new NoSuchElementException(String.format("Element is not found by '%s'", searchTextField_1.toString()));
+//        }
+//    }
+
     public void insertTextToTextFieldJS(String text) {
         try {
-            element = wait.until(ExpectedConditions.visibilityOfElementLocated(searchTextField));
-            System.out.println("Text before: " + element.getText());
-            String text2 = "arguments[0].setAttribute('value','" + text + "')";
-            js.executeScript(text2, element);
-            System.out.println("Text after: " + element.getText());
-
-
-//            String js1 = "arguments[0].setAttribute('value','"+text+"')";
-//            js.executeScript(js1, element);
-
-        } catch (NoSuchElementException | TimeoutException e) {
-            throw new NoSuchElementException(String.format("Element is not found by '%s'", searchTextField.toString()));
+            //JS click and type text
+            element = wait.until(ExpectedConditions.elementToBeClickable(searchTextField_2));
+            js.executeScript("arguments[0].click();", element);
+            js.executeScript("document.querySelector('[role=presentation] span[role]').value = 'AlphaSense'");
+            js.executeScript("arguments[0].click();", element);
+            Thread.sleep(10000);
+        } catch (NoSuchElementException | TimeoutException | InterruptedException e) {
+            throw new NoSuchElementException(String.format("Element is not found by '%s'", searchTextField_2.toString()));
         }
     }
 
     public void insertTextToTextFieldJS2(String text) {
         try {
-            element = wait.until(ExpectedConditions.visibilityOfElementLocated(searchTextField));
+            element = wait.until(ExpectedConditions.visibilityOfElementLocated(searchTextField_1));
             System.out.println("Text before: " + element.getText());
             js.executeScript("arguments[0].click();", element);
-            js.executeScript("document.querySelector('*[class*=CodeMirror-placeholder]').value='" + text + "'");
+            js.executeScript("document.querySelector('[role=presentation] span[role]').value='" + text + "'");
 
 //            js.executeScript("document.querySelector('.fusion-custom-menu-item-contents .s').value = 'SW Test Academy'");
 
@@ -72,7 +109,7 @@ public class AlphaSense {
             System.out.println("Text after: " + element.getText());
 
         } catch (NoSuchElementException | TimeoutException e) {
-            throw new NoSuchElementException(String.format("Element is not found by '%s'", searchTextField.toString()));
+            throw new NoSuchElementException(String.format("Element is not found by '%s'", searchTextField_1.toString()));
         }
     }
 
@@ -83,8 +120,9 @@ public class AlphaSense {
     }
 
     public void getTextFromElement () {
-        element = wait.until(ExpectedConditions.visibilityOfElementLocated(searchTextField));
+        element = wait.until(ExpectedConditions.visibilityOfElementLocated(searchTextField_1));
         System.out.println("Text issss: " + element.getText());
     }
+
 
 }
--- src/test/java/AlphaSensePageTest.java
+++ src/test/java/AlphaSensePageTest.java
@@ -1,12 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.*;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.AlphaSense;
-
-import java.time.Duration;
 
 public class AlphaSensePageTest {
 
@@ -20,11 +16,8 @@ public class AlphaSensePageTest {
     @Before
     public void beforeTest() {
         String url = "https://rc.alpha-sense.com/doc/PR-386ea743f2a90399fb0e4300ddf37d0697abc743";
-        long timeout = 10;
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout));
-//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
         driver.get(url);
     }
 
@@ -39,8 +32,7 @@ public class AlphaSensePageTest {
     @Test
     public void getTitle () {
         AlphaSense alphaSense = new AlphaSense(driver);
-        alphaSense.insertTextToTextFieldJS("AlphaSense");
-//        alphaSense.getTextFromElement();
+        alphaSense.sendTextToSearchBoxAndClick("AlphaSense");
     }
 
 }
